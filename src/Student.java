ce4c37dec77f2b5d34a04252e92dd3c496d2c653
Michael <michaelseifuaraya@gmail.com>
2025-01-01 16:53:48 +0100

    add student class

---
+++ src/Student.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+
+    private String studentId;
+    private String name;
+    private List<String> enrolledCourses;
+
+    public Student(String studentId, String name){
+        this.studentId = studentId;
+        this.name = name;
+        this.enrolledCourses = new ArrayList<>();
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getEnrolledCourses() {
+        return enrolledCourses;
+    }
+
+    public void enrollCourse(String courseId){
+        enrolledCourses.add(courseId);
+    }
+
+    public String toString(){
+        return "Student{" +
+                "studentId='" + studentId + '\'' +
+                ", name='" + name + '\'' +
+                ", entrolledCourses=" + enrolledCourses +
+                '}';
+    }
+}
